c894f60548a4bdf64ec1f98767e562ceeb258937
Kashaan-M <kashaanmahmood@gmail.com>
2022-12-21 17:46:14 +0500

    added joi

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,35 @@
+const Joi = require('joi').extend(require('@joi/date'));
 const User = require('../models/User');
 const Exercise = require('../models/Exercise');
 const { BadRequestError } = require('../errors');
 
-function prettifyDate(date) {
-  return date.toDateString();
-}
+// schema for joi. Need to validate 'from' and 'to'
+const joiDateSchema = Joi.date().format([
+  'YYYY-M-D',
+  'YYYY-MM-DD',
+  'DD-MM-YYYY',
+  'D-M-YYYY',
+  'DD/MM/YYYY',
+  'D/M/YYYY',
+  'YYYY/MM/DD',
+  'YYYY/M/D',
+  'YYYY MM DD',
+  'YYYY M D',
+  'DD MM YYYY',
+  'D M YYYY',
+  'YYYY.MM.DD',
+  'YYYY.M.D',
+  'DD.MM.YYYY',
+  'D.M.YYYY',
+  'DD,MM,YYYY',
+  'D,M,YYYY',
+  'YYYY,MM,DD',
+  'YYYY,M,D',
+]);
 
 const createUser = async (req, res) => {
   const { username } = req.body;
+  //console.log('createUser\n', 'req.body = ', req.body);
   const userExists = await User.find({ username });
   if (userExists.length === 1) {
     throw new BadRequestError('User already exists');
@@ -26,7 +48,9 @@ const getAllUsers = async (req, res) => {
 
 const createUserExercise = async (req, res) => {
   // destructuring. userId is alias
-  let { _id: userId, description, duration, date } = req.body;
+  //console.log('createUserExercise\n', 'req.body = ', req.body, ' req.params = ', req.params);
+  let { description, duration, date } = req.body;
+  let { id: userId } = req.params;
   duration = Number(duration);
   // simplistic validation for Date. A Date validation library could do better...
   if (date === '' || !date) {
@@ -35,7 +59,7 @@ const createUserExercise = async (req, res) => {
     date = new Date(date);
   }
 
-  const exerciseExist = await Exercise.find({ user: userId, description, duration });
+  const exerciseExist = await Exercise.find({ user: userId, description, duration, date });
   if (exerciseExist.length === 1) {
     throw new BadRequestError('Exercise already exists');
   }
@@ -49,57 +73,62 @@ const createUserExercise = async (req, res) => {
     { exercise: exerciseId },
     { new: true, runValidators: true },
   );
-  if (!user) {
-    // these two statements are not doing anything because this is a 'cast error' and the 'error-handler' middleware steps in to handle it.
-    // It gets handled in the end and that is what matters
-    const removeExercise = await Exercise.findByIdAndRemove({ exerciseId });
-    throw new BadRequestError(`No user with _id ${userId} exists`);
-  }
-
+  /*
+    if (!user) {
+      // these two statements are not doing anything because this is a 'cast error' and the 'error-handler' middleware steps in to handle it.
+      // It gets handled in the end and that is what matters
+      const removeExercise = await Exercise.findByIdAndRemove({ exerciseId });
+      throw new BadRequestError(`No user with _id ${userId} exists`);
+    }
+  */
   const userObj = await User.findById(userId).populate('exercise');
 
   return res.status(200).json({
-    username: userObj.username,
-    description: userObj.exercise.description,
-    duration: userObj.exercise.duration,
-    date: userObj.exercise.date.toDateString(),
     _id: userObj._id,
+    username: userObj.username,
+    date: userObj.exercise.date.toDateString(),
+    duration: userObj.exercise.duration,
+    description: userObj.exercise.description,
   });
 };
 
 const getUserLogs = async (req, res) => {
+  //console.log('getUserLogs\n', 'req.params = ', req.params, ' req.query = ', req.query);
   const { _id: userId } = req.params;
-  //console.log('req.query = ', req.query);
-  let { from, to, limit } = req.query;
-
-  //*** setting some default values for 'from', 'to' and 'limit'
-  //** Note : We can do better validation for 'from', 'to', 'limit' then this ...
-
-  // if `from` is not provided or empty string then make `from` the date '2000-1-1'
-  from ? (from = new Date(from)) : (from = new Date('2000-1-1'));
-
-  // if `to` is not provided or empty string then make `to` the current date
-  to ? (to = new Date(to)) : (to = new Date());
-
-  // if `limit` is not provided or empty string then make `limit` equal to integer 0. This makes mongoDB return all the exercises.
-  limit ? (limit = Number(limit)) : (limit = 0);
-
+  let { from, to } = req.query;
+  let fromWasValid = true;
+  let toWasValid = true;
+  const limit = Number(req.query.limit) || 0;
+  //console.log(joiDateSchema.validate(from));
+  const fromIsValid = from && !joiDateSchema.validate(from).hasOwnProperty('error');
+  if (!fromIsValid) {
+    fromWasValid = false;
+    from = new Date(0);
+  }
+  const toIsValid = to && !joiDateSchema.validate(to).hasOwnProperty('error');
+  if (!toIsValid) {
+    toWasValid = false;
+    to = new Date(Date.now());
+  }
   const user = await User.findById(userId);
   if (user === null || !user) {
     throw new BadRequestError(`No user with _id ${userId} exists.`);
   }
-
-  let exercises = await Exercise.find({ user: userId })
-    .where('date')
-    .gte(from)
-    .lte(to)
+  //console.log('from = ', from, ' to = ', to, ' limit = ', limit);
+  let exercises = await Exercise.find({
+    user: userId,
+    date: { $gte: from, $lte: to },
+  })
     .select({
       description: 1,
       duration: 1,
       date: 1,
       _id: 0,
     })
-    .limit(limit);
+    .limit(limit)
+    .sort({ date: 'desc' });
+  //console.log('exercises = ', exercises);
+
   if (exercises.length === 0 || !exercises) {
     throw new BadRequestError('User has not added any exercises');
   }
@@ -112,7 +141,61 @@ const getUserLogs = async (req, res) => {
     return { description, duration, date };
   });
 
-  return res.status(200).json({ username, count, _id, log: [...prettyExercises] });
+  let queries = Object.keys(req.query);
+
+  if (
+    queries.includes('from') &&
+    fromWasValid &&
+    queries.includes('to') &&
+    toWasValid &&
+    queries.includes('limit')
+  ) {
+    return res.status(200).json({
+      _id,
+      username,
+      from: new Date(from).toDateString(),
+      to: new Date(to).toDateString(),
+      count,
+      log: [...prettyExercises],
+    });
+  } else if (queries.includes('from') && fromWasValid && queries.includes('to') && toWasValid) {
+    return res.status(200).json({
+      _id,
+      username,
+      from: new Date(from).toDateString(),
+      to: new Date(to).toDateString(),
+      count,
+      log: [...prettyExercises],
+    });
+  } else if (queries.includes('from') && fromWasValid && queries.includes('limit')) {
+    return res.status(200).json({
+      _id,
+      username,
+      from: new Date(from).toDateString(),
+      count,
+      log: [...prettyExercises],
+    });
+  } else if (queries.includes('to') && toWasValid && queries.includes('limit')) {
+    return res
+      .status(200)
+      .json({ _id, username, to: new Date(to).toDateString(), count, log: [...prettyExercises] });
+  } else if (queries.includes('from') && fromWasValid) {
+    return res.status(200).json({
+      _id,
+      username,
+      from: new Date(from).toDateString(),
+      count,
+      log: [...prettyExercises],
+    });
+  } else if (queries.includes('to') && toWasValid) {
+    return res
+      .status(200)
+      .json({ _id, username, to: new Date(to).toDateString(), count, log: [...prettyExercises] });
+  } else if (queries.includes('limit')) {
+    return res.status(200).json({ _id, username, count, log: [...prettyExercises] });
+  } else {
+    return res.status(200).json({ _id, username, count, log: [...prettyExercises] });
+  }
 };
 
 module.exports = { getAllUsers, createUser, createUserExercise, getUserLogs };
